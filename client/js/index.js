78c0937667471bc78c96cbed0ac5fde08246d4d7
Robin Biju Thomas <robinbiju4710@gmail.com>
2019-03-12 06:15:25 +0530

    Added OSM

--- client/js/index.js
+++ client/js/index.js
@@ -1,34 +1,64 @@
 var socket = io();
+var list_id = 0;
 
 
-socket.on('new-song', function(msg) {
 
-        console.log('message: ' + msg);
-	
-	var trim_msg = msg.substring(0, 50);
+socket.on('alerts', function(msg) {
 
-	var node = document.createElement("LI");   
-	node.classList.add("collection-item");
+    console.log('message: ' + msg);
+    var trim_msg = msg.substring(0, 50);
 
-	node.innerHTML = trim_msg + "<span class=\"new badge\" data-badge-caption=\"remove\" onclick=\"savelink()\"/>"                     
-	document.getElementById("sbox").appendChild(node);
+    list_id++;
+    var a = list_id.toString();
+    x = 'list_item_' + a;
+    y = 'list_btn_' + a;
+    console.log('a= ' + a);
 
 
+    var node = document.createElement("LI");
+    node.classList.add("collection-item");
+    node.setAttribute("id", x);
+
+
+    node.innerHTML = trim_msg + "<span class=\"new badge\" data-badge-caption=\"Raise Ticket\" id=" + y + " onclick=\"raise_ticket(id)\"/>"
+    document.getElementById("abox").appendChild(node);
 
 });
 
 
-function savelink() {
+socket.on('r_tickets', function(msg) {
 
-    var link = document.getElementById("link-input").value;
-    console.log(link);
-    socket.emit('vid-link', link);
-    document.getElementById("link-input").value = "";
+    console.log('message: ' + msg);
+    var trim_msg = msg.substring(0, 50);
+
+    var node = document.createElement("LI");
+    node.classList.add("collection-item");
+    node.setAttribute("id", x);
+
+
+    node.innerHTML = trim_msg;
+    document.getElementById("tbox").appendChild(node);
+
+});
+
+
+
+function raise_ticket(rem_id) {
+
+    var name = document.getElementById(rem_item).textContent;
+    var t_id = name.substring(0, 4);
+    socket.emit('tickets', t_id);
 }
 
-function dwd() {
+function showMap() {
+    var mymap = L.map('mapid').setView([12.823109, 80.041021], 13);
 
-    console.log("Sending Download Trigger");
-    socket.emit('download', "download");
-    document.getElementById("link-input").value = "The Videos are now downloading...";
+    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoidGVrbmljdXMiLCJhIjoiY2p0NTB5ZXZyMDFqNzQ5bzZ5ZDBodXViYyJ9.SuiIDJWFCq7kPqdXD9i67w', {
+        maxZoom: 18,
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+            '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+            'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        id: 'mapbox.streets'
+    }).addTo(mymap);
+    var marker = L.marker([12.823109, 80.041021]).addTo(mymap);
 }
